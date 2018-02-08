69740a5353c99ba580ceb89c999060df00a50c84
Sai Chaitanya <saichaitanya88@gmail.com>
2018-02-07 21:09:24 -0500

    added queue using two stacks

---
+++ src/StacksAndQueues/MyQueue.java
@@ -0,0 +1,12 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+// Implement a queue with two stacks
+public class MyQueue {
+    public static void main(String[] args){
+
+    }
+    Stack<Integer> s1, s2;
+
+}
