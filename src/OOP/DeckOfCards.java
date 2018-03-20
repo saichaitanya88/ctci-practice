89ee5d9bd3f0e41b43fc379e646e6148ef894723
Sai Chaitanya <saichaitanya88@gmail.com>
2018-03-20 07:36:51 -0400

    generic deck of cards added

--- src/OOP/DeckOfCards.java
+++ src/OOP/DeckOfCards.java
@@ -5,3 +5,23 @@ public class DeckOfCards {
 
     }
 }
+
+
+
+class Card{
+    public enum Suit{
+        CLUBS (1), SPADES (2), HEARTS (3), DIAMONDS (4);
+        int value;
+        private Suit(int v){
+            value = v;
+        }
+    }
+    private int card;
+    private Suit suit;
+    public Card(int c, Suit s){
+        card = c;
+        suit = s;
+    }
+    public int value() { return card; }
+    public Suit suit() { return suit; }
+}
