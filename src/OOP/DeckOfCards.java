79b7f5e46b196ab6f12f3a1aa46c163338b8e26d
Sai Chaitanya <saichaitanya88@gmail.com>
2018-03-21 05:46:57 -0400

    deck of cards with blackjack class

--- src/OOP/DeckOfCards.java
+++ src/OOP/DeckOfCards.java
@@ -25,3 +25,19 @@ class Card{
     public int value() { return card; }
     public Suit suit() { return suit; }
 }
+
+class BlackJackCard extends Card{
+    public BlackJackCard(int r, Suit s){
+        super(r,s);
+    }
+    public int value(){
+        int r = super.value();
+        if (isAce())
+            return 11; // Aces are 11
+
+        return r;
+    }
+    public boolean isAce(){
+        return super.value() == 1;
+    }
+}
